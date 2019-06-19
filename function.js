2c22434c716fa149b0578de26fee0a3abb69bf15
Youtirsin <171122318@qq.com>
2019-06-19 09:57:51 +0800

    load the gif before the page

--- function.js
+++ function.js
@@ -1,73 +1,75 @@
-function preload(){
-    var imgs= new Array();
-    for (var i=0;i<preload.arguments.length;i++){
-        imgs[i]= new Image();
-        imgs[i].src=preload.arguments[i];
+function preload() {
+    var imgs = new Array();
+    for (var i = 0; i < preload.arguments.length; i++) {
+        imgs[i] = new Image();
+        imgs[i].src = preload.arguments[i];
     }
 }
-if(!IsPC()){
-    window.location.href='mobile.html';
-}else{
-    $(document).ready(function () {
-        $('.piece').each(function (index) {
-            $(this).css({
-                'left': parseInt($('#container').css('left')) + index % 4 * 150,
-                'top': parseInt($('#container').css('top')) + parseInt(index / 4) * 84
-            });
-            $(this).click(() => {
-                if ($(this).html() == '') {
-                    i = index % 4;
-                    j = parseInt(index / 4);
-                    $(this).html('<img src="pieces/cat' + i + j + '.png"/>');
-                } else {
-                    $(this).html('');
-                }
-                allshowed = true;
-                for (var i = 0; i < 4; i++) {
-                    for (var j = 0; j < 4; j++) {
-                        if ($('.piece').eq(i * 4 + j).html() == '') {
-                            allshowed = false;
+if (!IsPC()) {
+    window.location.href = 'mobile.html';
+} else {
+    $.get('cat.gif', function () {
+        $(document).ready(function () {
+            $('#note').html("<h1>Click the square</h1>");
+            $('.piece').each(function (index) {
+                $(this).css({
+                    'left': parseInt($('#container').css('left')) + index % 4 * 150,
+                    'top': parseInt($('#container').css('top')) + parseInt(index / 4) * 84
+                });
+                $(this).click(() => {
+                    if ($(this).html() == '') {
+                        i = index % 4;
+                        j = parseInt(index / 4);
+                        $(this).html('<img src="pieces/cat' + i + j + '.png"/>');
+                    } else {
+                        $(this).html('');
+                    }
+                    allshowed = true;
+                    for (var i = 0; i < 4; i++) {
+                        for (var j = 0; j < 4; j++) {
+                            if ($('.piece').eq(i * 4 + j).html() == '') {
+                                allshowed = false;
+                            }
                         }
                     }
-                }
-                if (allshowed) {
-                    $('#note').html("<h1>This is my sister's cat</h1><h1>Pretty cute,huh?</h1>");
-                    $('#container').html('<img src="cat.gif" id="gif"/>');
-                    $('#gif').css({
-                        'left': $('#container').css('left'),
-                        'top': $('#container').css('top'),
-                        'opacity':1,
-                        'transition':'opacity 4s'
-                    });
-                }
+                    if (allshowed) {
+                        $('#note').html("<h1>This is my sister's cat</h1><h1>Pretty cute,huh?</h1>");
+                        $('#container').html('<img src="cat.gif" id="gif"/>');
+                        $('#gif').css({
+                            'left': $('#container').css('left'),
+                            'top': $('#container').css('top'),
+                            'opacity': 1,
+                            'transition': 'opacity 4s'
+                        });
+                    }
+                });
             });
         });
     });
-    
+
     preload(
-        'cat.gif',
-        
         'pieces/cat00.png',
         'pieces/cat01.png',
         'pieces/cat02.png',
         'pieces/cat03.png',
-        
+
         'pieces/cat10.png',
         'pieces/cat11.png',
         'pieces/cat12.png',
         'pieces/cat13.png',
-    
+
         'pieces/cat20.png',
         'pieces/cat21.png',
         'pieces/cat22.png',
         'pieces/cat23.png',
-    
+
         'pieces/cat30.png',
         'pieces/cat31.png',
         'pieces/cat32.png',
         'pieces/cat33.png'
     );
 }
+
 function IsPC() {
     var userAgentInfo = navigator.userAgent;
     var Agents = ["Android", "iPhone",
